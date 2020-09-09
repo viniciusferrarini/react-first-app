741882acef7a2a7d6fc935c4019f7c0f89580189
Vinicius Ferrarini <vinicius.ferrarini@softfocus.com.br>
2020-09-09 01:39:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
+import './assets/app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			notas: []
+		};
+	}
+
+	criarNota(titulo, texto) {
+		const novaNota = {titulo, texto}
+		const novoArrayNotas = [...this.state.notas, novaNota];
+		const novoEstado = {
+			notas: novoArrayNotas
+		}
+		this.setState(novoEstado);
+	}
+
+	render() {
+		return ( 
+			<section className="conteudo">
+				<FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+				<ListaDeNotas notas={this.state.notas} />
+			</section>
+		);
+	}
 }
 
+//react - lib do facebook
+//React - todo ecosistema do react
 export default App;
